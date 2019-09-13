47930a48b44f4b566ecfdab8328fc44a08d3c380
simran <macstudent@10.10.150.116>
2019-09-13 11:23:58 -0400

    exception class

--- tests/OperationNotAllowedException.java
+++ tests/OperationNotAllowedException.java
@@ -1,5 +1,8 @@
 
 public class OperationNotAllowedException extends Exception {
+	public String getMessage() {
+		return "Add Book operation";
+	}
 
 	public Object getOperation() {
 		// TODO Auto-generated method stub
